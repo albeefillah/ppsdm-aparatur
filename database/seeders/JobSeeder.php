a0252ea2c2657ff57363ac2b22bc3ef558a6f55f
Albee Akbar Fillah <albeefillah17@gmail.com>
2025-04-20 19:15:25 +0700

    update seeder

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -23,19 +23,22 @@ class JobSeeder extends Seeder
             ['code' => 'K2', 'name' => 'KANTOR LT. 2', 'category' => 'cs', 'type' => 'primary', 'shift' => 'pagi', 'start' => '07:00', 'end' => '17:00'],
             ['code' => 'K3', 'name' => 'KANTOR LT. 3', 'category' => 'cs', 'type' => 'primary', 'shift' => 'pagi', 'start' => '07:00', 'end' => '17:00'],
             ['code' => 'K4', 'name' => 'KANTOR LT. 4', 'category' => 'cs', 'type' => 'primary', 'shift' => 'pagi', 'start' => '07:00', 'end' => '17:00'],
+            ['code' => 'FOP', 'name' => 'FRONT OFFICE - PAGI', 'category' => 'cs', 'type' => 'primary', 'shift' => 'pagi', 'start' => '07:30', 'end' => '19:30'],
 
             // Special jobs
             ['code' => 'GDB', 'name' => 'BASEMENT + GARDEN WISMA', 'category' => 'marbot', 'type' => 'special', 'shift' => 'pagi', 'start' => '07:00', 'end' => '17:00'],
             ['code' => 'GD', 'name' => 'GARDEN', 'category' => 'garden', 'type' => 'special', 'shift' => 'pagi', 'start' => '07:00', 'end' => '17:00'],
             ['code' => 'RRP', 'name' => 'RESTROOM PUBLIC WANITA', 'category' => 'women', 'type' => 'special', 'shift' => 'pagi', 'start' => '07:00', 'end' => '17:00'],
-            ['code' => 'BQ', 'name' => 'BANQUET', 'category' => 'banquet', 'type' => 'special', 'shift' => 'pagi', 'start' => '07:01', 'end' => '17:01'],
-            ['code' => 'FOP', 'name' => 'FRONT OFFICE - PAGI', 'category' => 'koor', 'type' => 'special', 'shift' => 'pagi', 'start' => '07:30', 'end' => '19:30'],
+            ['code' => 'BQ', 'name' => 'BANQUET', 'category' => 'koor', 'type' => 'special', 'shift' => 'pagi', 'start' => '07:01', 'end' => '17:01'],
+            // ['code' => 'FOP', 'name' => 'FRONT OFFICE - PAGI', 'category' => 'koor', 'type' => 'special', 'shift' => 'pagi', 'start' => '07:30', 'end' => '19:30'],
+
 
             // Secondary jobs
             ['code' => 'LDP', 'name' => 'LONDRY - PAGI', 'category' => 'cs', 'type' => 'secondary', 'shift' => 'pagi', 'start' => '07:00', 'end' => '17:00'],
             ['code' => 'FOM', 'name' => 'FRONT OFFICE - MALAM', 'category' => 'cs', 'type' => 'secondary', 'shift' => 'malam', 'start' => '19:30', 'end' => '07:30'],
             ['code' => 'OBM', 'name' => 'OFFICE BOY - MALAM', 'category' => 'cs', 'type' => 'secondary', 'shift' => 'malam', 'start' => '19:30', 'end' => '07:30'],
             ['code' => 'LDS', 'name' => 'LONDRY - SIANG', 'category' => 'cs', 'type' => 'secondary', 'shift' => 'malam', 'start' => '10:00', 'end' => '19:00'],
+
         ];
 
         foreach ($jobs as $job) {
