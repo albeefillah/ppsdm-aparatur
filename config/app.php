6b88293efbde18d847906db1e86334f84e45e568
Albee Akbar Fillah <albeefillah17@gmail.com>
2025-05-23 14:14:35 +0700

    barrypdf conffigure

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -185,6 +186,8 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
         'FastExcel' => Rap2hpoutre\FastExcel\Facades\FastExcel::class,
+        'PDF' => Barryvdh\DomPDF\Pdf::class,
+
     ])->toArray(),
 
 ];
